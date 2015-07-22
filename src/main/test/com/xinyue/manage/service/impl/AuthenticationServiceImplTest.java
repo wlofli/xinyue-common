92ba9cafcac91021879e4d593a8679d48f6f0d59
ice-wolf <18768104912@163.com>
2015-07-22 21:40:44 +0800

    ..
    ..

---
+++ src/main/test/com/xinyue/manage/service/impl/AuthenticationServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.xinyue.manage.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * author lzc
+ * 2015年7月19日下午11:15:52
+ */
+public class AuthenticationServiceImplTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testGetIndustryList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthenticationInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDetailByCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExportData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateAuthenticationStatusByCode() {
+		fail("Not yet implemented");
+	}
+
+}
